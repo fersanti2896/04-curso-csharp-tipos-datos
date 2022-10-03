0c6f26458de875eba94fa1891ea9d19620bd391d
Fernando Nicolás <fersanti2896@gmail.com>
2022-10-03 10:33:03 -0500

    Tipo toString en 01-toString.cs

---
+++ Codigo/01-toString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiposDeDatos.Codigo {
+    public class _01_toString {
+        public void toString() {
+            var num = 25;
+
+            if (num.ToString() == "5") { 
+                Console.WriteLine("Valores iguales");
+            } else {
+                Console.WriteLine("Valores distintos");
+            }
+
+            var fecha = DateTime.Today.ToString("yy-MM-dd dddd");
+            Console.WriteLine(fecha);
+        }
+    }
+}
